c9b7b28cd39b80f4e4f5b65b022560b85d992d72
17015EmilijaBicanin <emilija.b@elfak.rs>
2022-01-10 22:54:26 +0100

    update 8

--- ProjekatBaze (1)/ProjekatBaze/findme/src/App.js
+++ ProjekatBaze (1)/ProjekatBaze/findme/src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
+import {useState,useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import Topbar from './components/Topbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Pocetna from './stranice/Pocetna/Pocetna'
 import PrijaviSe from './stranice/PrijaviSe/PrijaviSe'
-
+import { HubConnectionBuilder } from '@microsoft/signalr';
 import UspesnoRegistrovanje from './stranice/PrijaviSe/UspesnoRegistrovanje'
 import Profil from './stranice/Profil/Profil'
 import Novosti from './stranice/Novosti/Novosti'
@@ -17,21 +18,61 @@ import Fotografije from './stranice/Fotografije/Fotografije'
 import Informacije from './stranice/Informacije/Informacije'
 import Pratioci from './stranice/Pratioci/Pratioci'
 import Prati from './stranice/Prati/Prati'
+import { ToastProvider, useToasts } from 'react-toast-notifications';
 function App() {
+  const [username,setUsername]=useState("")
+  const [myConnection,setMyConnection]=useState(null)
+  const [notification,setNotifikacija]=useState({})
+  const { addToast } = useToasts()
+  useEffect(()=>{
+    const connect=()=>{const newConnection=new HubConnectionBuilder()
+        
+       
+    .withUrl('http://localhost:5000/hubs/chat?username='+localStorage.getItem('username')
+        
+    )
+    .withAutomaticReconnect()
+    .build();
+    newConnection.start()
+    .then(result=>{
+        console.log('connect');
+        newConnection.invoke("GetConnectionId").then(rez=>{console.log(rez)})
+    
+        newConnection.on('ReceiveNotification',notifikacija=>{
+           
+          //setMessages(prevState=>[...prevState,message])
+          console.log("stigla notifikacija")
+          setNotifikacija(notifikacija)
+            addToast(notifikacija.text,{
+              appearance:'info',
+              autoDismiss:true
+            })
+
+        });
+    })
+    .catch(e=>console.log('ne valja',e));
+    setMyConnection(newConnection)}
+
+    connect()
+    
+
+      
+  },[username])
   return (
    <div>
   
    <>
+ 
    <Store>
       <Router>
         <Navbar />
         <Topbar/>
         <Routes>
         <Route path='/' element={<Pocetna/>}/>
-        <Route path='/prijaviSe' element={<PrijaviSe/>}/>
+        <Route path='/prijaviSe' element={<PrijaviSe setUsernam={setUsername}/>}/>
         <Route path='/registracija/*' element={<Registracija/>}/>
         <Route path='/Fotografije' element={<Fotografije/>}/>
-        <Route path='/Informacije' element={<Informacije/>}/>
+         <Route path='/Informacije' element={<Informacije/>}/>
         <Route path='/Pratioci' element={<Pratioci/>}/>
         <Route path='/Prati' element={<Prati/>}/>
         <Route path='/UspesnoRegistrovanje' element={<UspesnoRegistrovanje/>}/>
@@ -43,6 +84,7 @@ function App() {
       
       </Router>
       </Store>
+    
     </>
    </div> 
   )

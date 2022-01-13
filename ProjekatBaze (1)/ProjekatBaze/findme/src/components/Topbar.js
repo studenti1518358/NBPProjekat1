fc8567ab0da87ef0cd79455c917e1d08b4d0eea5
17015EmilijaBicanin <emilija.b@elfak.rs>
2022-01-13 15:00:23 +0100

    nesto

--- ProjekatBaze (1)/ProjekatBaze/findme/src/components/Topbar.js
+++ ProjekatBaze (1)/ProjekatBaze/findme/src/components/Topbar.js
@@ -3,8 +3,9 @@ import React,{useState,useEffect,useMemo,useContext,useRef} from 'react'
 import {NavLink, useNavigate} from "react-router-dom"
 import Search from './Search'
 import {Context} from '../context/Store'
-import Obavestenja from "./Obavestenja"
-export default function Topbar() {
+
+import Notifikacije from './Notifikacije'
+export default function Topbar(props) {
   const [state,dispatch]=useContext(Context);
   const [showNotifications,setShowNotifications]=useState(false)
   const navigate=useNavigate()
@@ -53,7 +54,7 @@ export default function Topbar() {
    const [prikaziDiv,setPrikaziDiv]=useState(false)
    const [pretraga,setPretraga]=useState([])
    const [pretrazi,setPretrazi] =useState("")
-   const obavestenjaRef = useRef(null);
+  
   
    useEffect(() => {
      setProfilnaSrc(localStorage.getItem("profilna"))
@@ -124,11 +125,9 @@ const [isLogged,setIsLogged]=useState(true)
             <span className="topbarIconBadge">1</span>
           </div>
           <div className="topbarIconItem">
-          <svg onClick={()=>setShowNotifications(true)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell ikonicaBar" viewBox="0 0 16 16">
-          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
-         </svg>
-        {showNotifications && <Obavestenja ref={obavestenjaRef}></Obavestenja>}
-            <span className="topbarIconBadge">1</span>
+          
+         <Notifikacije novaNotifikacija={props.novaNotifikacija} />
+        
           </div>
           <div className="topbarIconItem">
             <NavLink to="/Chat">
